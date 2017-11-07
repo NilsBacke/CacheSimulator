e19717c512ace8b201d1b232694630ff16de1bbc
NilsBacke <nils.backe@gmail.com>
2017-11-06 22:46:38 -0500

    removed LinkedList

--- src/ExampleDataProvider.java
+++ src/ExampleDataProvider.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class ExampleDataProvider<T, U> implements DataProvider<T, U>{
 
-    private ArrayList<LRUCache.ListElement> _list;
+    private ArrayList<ListElement<T, U>> _list;
     private int _numFetches;
 
     // keys and values must be the same lengths
@@ -10,13 +10,13 @@ public class ExampleDataProvider<T, U> implements DataProvider<T, U>{
         _numFetches = 0;
         _list = new ArrayList<>();
         for (int i = 0; i < keys.size(); i++) {
-            _list.add(new LRUCache.ListElement(keys.get(i), values.get(i)));
+            _list.add(new ListElement<>(keys.get(i), values.get(i)));
         }
     }
 
     public U get(T key) {
         _numFetches++;
-        for (LRUCache.ListElement ele : _list) {
+        for (ListElement ele : _list) {
             if (ele.getKey().equals(key)) {
                 return (U) ele.getValue();
             }
@@ -26,5 +26,24 @@ public class ExampleDataProvider<T, U> implements DataProvider<T, U>{
 
     public int getNumFetches() {
         return _numFetches;
+    }
+
+    private static class ListElement<T, U> {
+
+        private T key;
+        private U value;
+
+        public ListElement(T key, U value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public U getValue() {
+            return value;
+        }
     }
 }
--- src/LRUCache.java
+++ src/LRUCache.java
@@ -7,9 +7,9 @@ import java.util.*;
 public class LRUCache<T, U> implements Cache<T, U> {
 
 	private int _numMisses, _capacity;
-	private Map<T, ListElement<T, U>> _hashmap; // weakest type
-	private Deque<ListElement<T, U>> _linkedList; // weakest type
+	private Map<T, Node<T, U>> _hashmap; // weakest type
 	private DataProvider<T, U> _dataProvider;
+	private Node _head, _tail;
 
 	/**
 	 * @param provider the data provider to consult for a cache miss
@@ -17,10 +17,11 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	 */
 	public LRUCache (DataProvider<T, U> provider, int capacity) {
 		_numMisses = 0;
-		_hashmap = new HashMap<>();
-		_linkedList = new LinkedList<>();
+		_hashmap = new HashMap<>(capacity);
 		_dataProvider = provider;
 		_capacity = capacity;
+		_head = null;
+		_tail = null;
 	}
 
 	/**
@@ -29,27 +30,28 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	 * @return the value associated with the key
 	 */
 	public U get (T key) {
-		ListElement element;
+		Node node;
 		// if cache hit
 		if (_hashmap.containsKey(key)) {
-			 element = _hashmap.get(key);
-			_linkedList.remove(element);
-			_linkedList.addFirst(element);
+			node = _hashmap.get(key); // gets the requested node
+			remove(node); // removes the requested node from the linked list
+			addToFront(node); // adds the requested node to the front of the linked list
 		} else { // if cache miss
 			_numMisses++;
-			final U value = _dataProvider.get(key);
-			element = new ListElement<>(key, value);
-			_hashmap.put(key, element);
-			_linkedList.addFirst(element);
+			final U value = _dataProvider.get(key); // get value from data provider
+			node = new Node<>(key, value); // instantiate a new node with the requested key and returned value
+			_hashmap.put(key, node); // adds the new node to the hashmap
+			addToFront(node); // adds the new node to the front of the linked list
 		}
 
 		// if cache is full
-		if (_linkedList.size() > _capacity) {
-			final ListElement<T, U> lastElement = _linkedList.removeLast();
-			_hashmap.remove(lastElement.getKey());
+		if (_hashmap.size() > _capacity) {
+			final Node nodeBeingRemoved = _tail;
+			remove(_tail); // remove the LRU node from the linked list
+			_hashmap.remove(nodeBeingRemoved.getKey()); // remove that same node from the hashmap
 		}
 
-		return (U) element.getValue();
+		return (U) node.getValue();
 	}
 
 	/**
@@ -60,22 +62,54 @@ public class LRUCache<T, U> implements Cache<T, U> {
 		return _numMisses;
 	}
 
-	public static class ListElement<T, U> { // public because ExampleDataProvider utilizes it
-
-		private T key;
-		private U value;
-
-		public ListElement(T key, U value) {
-			this.key = key;
-			this.value = value;
-		}
-
-		public T getKey() {
-			return key;
-		}
-
-		public U getValue() {
-			return value;
+	/**
+	 * Adds the given node to the front of the doubly linked list
+	 * @param node The node that will be added to the front of the doubly linked list
+	 */
+	private void addToFront(Node node) {
+		if (_head == null) {
+			_head = node;
+		} else {
+			Node newSecondNode = _head;
+			_head = node;
+			newSecondNode._prev = node;
+			_head._next = newSecondNode;
 		}
 	}
+
+	/**
+	 * Removes the given node from the doubly linked list
+	 * @param node The node intended to be removed from the doubly linked list
+	 */
+	private void remove(Node node) {
+		if (node == _head) {
+			_head = null;
+		} else {
+			node._prev._next = node._next;
+			node._next._prev = node._prev;
+		}
+	}
+
+	private class Node<T, U> {
+
+		private T _key;
+		private U _value;
+		Node<T, U> _next, _prev;
+
+		private Node(T key, U value) {
+			this._key = key;
+			this._value = value;
+			this._prev = null;
+			this._next = null;
+		}
+
+		private T getKey() {
+			return _key;
+		}
+
+		private U getValue() {
+			return _value;
+		}
+	}
+
 }
